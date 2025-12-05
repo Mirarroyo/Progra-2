31e0c5d6124040def3e397252a9cbc070809c389
Miranda Arroyo <a01710117@tec.mx>
2025-12-05 05:48:49 -0600

    comentarios y clases update

--- PrestamoEbook.h
+++ PrestamoEbook.h
@@ -1,6 +1,10 @@
+/**
+ * clases de agregación
+ * registra préstamos de ebooks a alumnos
+ * relaciona instancias de Ebook y Alumno
+ */
 #ifndef PRESTAMOEBOOK_H_
 #define PRESTAMOEBOOK_H_
-
 #include "Alumno.h"
 #include "Ebook.h"
 
@@ -11,12 +15,27 @@ private:
     int dias;
 
 public:
-    // Constructores y métodos
+/**
+ * constructores con parámetros
+ * inicializa el estado de los atributos
+ * @param l Ebook prestado
+ * @param a Alumno que realiza el préstamo
+ * @param d Días del préstamo
+ */
+    // Constructor
     PrestamoEbook();
     PrestamoEbook(Ebook l, Alumno a, int d);
 
+    //Setter y Getter
     void setDias(int d);
     int getDias() const;
+
+    /**
+     * genera reporte del préstamo en formato string
+     * @return string con la info del préstamo
+     * llama a toString() de Ebook y Alumno
+     */
+    // Método
     string toString() const;
 };
 
@@ -32,15 +51,21 @@ PrestamoEbook::PrestamoEbook(Ebook l, Alumno a, int d) {
     dias = d;
 }
 
-void PrestamoEbook::setDias(int d) { dias = d; }
-int PrestamoEbook::getDias() const { return dias; }
-
+void PrestamoEbook::setDias(int d) {
+     dias = d; 
+}
+int PrestamoEbook::getDias() const {
+     return dias;
+}
+/**
+ * Este método implementa la agregacion mediante la 
+ * tecnica de delegación, llamando a los métodos toString()
+ * de los objetos Ebook y Alumno que forman parte del préstamo.
+ */
 string PrestamoEbook::toString() const {
     stringstream aux;
-    aux << "Préstamo de Ebook\n"
-        << alumno.toString() << "\n"
-        << libro.toString() << "\n"
-        << "Días de préstamo: " << dias;
+    aux << "Préstamo a: " << alumno.toString() 
+    << " de: " << libro.toString() << "Por: " << dias << " días.";
     return aux.str();
 }
 
