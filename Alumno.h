aff12ef4ec1c168859848edb20707359382c1517
Miranda Arroyo <a01710117@tec.mx>
2025-12-05 09:37:06 -0600

    Mejora de formato y comentarios
    Refactor getters and setters for better readability.

--- Alumno.h
+++ Alumno.h
@@ -1,6 +1,9 @@
+/**
+ * Representa al usuario en este programa 
+ * sus instancias son agregadas por las clases de préstamo 
+ */
 #ifndef ALUMNO_H_
 #define ALUMNO_H_
-
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -9,24 +12,36 @@ using namespace std;
 
 class Alumno {
 private:
+    // Atributos
     string nombre;
     string matricula;
 
 public:
-    // Constructores y métodos
+    // Constructor default
     Alumno();
+    /**
+     * Constructor con parámetros
+     * @param n Nombre del alumno
+     * @param m Matrícula del alumno
+     */
+
     Alumno(string n, string m);
 
-    // Getters y Setters
+    // Getters para obteneter un atributo priado
     string getNombre() const;
     string getMatricula() const;
+    // Setters para modificar un atributo privado
     void setNombre(string n);
     void setMatricula(string m);
 
+    /**
+     * @return cadena string con info del alumno
+     * Una representación en cadena del objeto alumno
+     */
     string toString() const;
 };
 
-// Implementación
+// Implementación de funciones 
 
 Alumno::Alumno() {
     nombre = "";
@@ -39,16 +54,24 @@ Alumno::Alumno(string n, string m) {
 }
 
 // Getters
-string Alumno::getNombre() const { return nombre; }
-string Alumno::getMatricula() const { return matricula; }
+string Alumno::getNombre() const { 
+    return nombre; 
+}
+string Alumno::getMatricula() const {
+     return matricula; 
+    }
 
 // Setters
-void Alumno::setNombre(string n) { nombre = n; }
-void Alumno::setMatricula(string m) { matricula = m; }
-
+void Alumno::setNombre(string n) {
+     nombre = n; 
+    }
+void Alumno::setMatricula(string m) {
+     matricula = m;
+     }
+// Método
 string Alumno::toString() const {
     stringstream aux;
-    aux << "Alumno: " << nombre << " (" << matricula << ")";
+    aux << "Alumno: " << nombre << " , " << matricula;
     return aux.str();
 }
 
