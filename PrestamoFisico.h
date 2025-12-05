6d533733121befb0f8338196596323371827a405
Miranda Arroyo <a01710117@tec.mx>
2025-12-05 09:47:15 -0600

    mejora de comentarios
    Refactor PrestamoFisico class to improve readability and organization.

--- PrestamoFisico.h
+++ PrestamoFisico.h
@@ -1,26 +1,46 @@
+/**
+ * clase de agregación
+ * registra préstamos de libros físicos a alumnos
+ * relaciona instancias de LibroFisico y Alumno
+ */
 #ifndef PRESTAMOFISICO_H_
 #define PRESTAMOFISICO_H_
-
 #include "Alumno.h"
 #include "LibroFisico.h"
 
 class PrestamoFisico {
 private:
+// Atributos
     Libro libro; 
     Alumno alumno;     
     int dias;
 
 public:
-    // Constructores y métodos
+    // Constructor default 
     PrestamoFisico();
+    /**
+     * Constructor con parámetros
+     * inicializa el estado de los atributos
+     * @param l Libro prestado
+     * @param a Alumno que realiza el préstamo
+     * @param d Días del préstamo
+     */
     PrestamoFisico(Libro l, Alumno a, int d);
 
+    //Setter y Getter
     void setDias(int d);
     int getDias() const;
+
+    // Método
+    /**
+     * genera reporte del préstamo en formato string
+     * @return string con la info del préstamo
+     * llama a toString() de Libro y Alumno
+     */
     string toString() const;
 };
 
-// Implementación
+// Implementación de las funnciones 
 
 PrestamoFisico::PrestamoFisico() : libro(), alumno() {
     dias = 0;
@@ -32,15 +52,21 @@ PrestamoFisico::PrestamoFisico(Libro l, Alumno a, int d) {
     dias = d;
 }
 
-void PrestamoFisico::setDias(int d) { dias = d; }
-int PrestamoFisico::getDias() const { return dias; }
-
+void PrestamoFisico::setDias(int d) { 
+    dias = d;
+}
+int PrestamoFisico::getDias() const {
+     return dias; 
+}
+/**
+ * Este método implementa la agregacion mediante la 
+ * tecnica de delegación, llamando a los métodos toString()
+ * de los objetos libro fisico y Alumno que forman parte del préstamo.
+ */
 string PrestamoFisico::toString() const {
     stringstream aux;
-    aux << "Préstamo de Fisico\n"
-        << alumno.toString() << "\n"
-        << libro.toString() << "\n"
-        << "Días de préstamo: " << dias;
+    aux << "Préstamo a: " << alumno.toString() 
+    <<" de: "<< libro.toString() << "Por: " << dias << " días.";
     return aux.str();
 }
 
