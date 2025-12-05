57bc9d3577236e3ba106f209641ed15c715da256
Miranda Arroyo <a01710117@tec.mx>
2025-12-05 05:14:53 -0600

    Commentarios y origanización de la clase

--- Libro.h
+++ Libro.h
@@ -1,32 +1,51 @@
+/**
+ * Clase Padre: Libro
+ * contiene atributos y métodos comunes para los tipos de libros.
+ */
 #ifndef LIBRO_H_
 #define LIBRO_H_
-
 #include <string>
 #include <sstream>
-
 using namespace std;
 
 class Libro {
-private:
+protected:
+// Artibutos en protected para que las clases hijas puedan acceder
     string titulo;
     string autor;
     int anio;
 
 public:
-    // Constructores y métodos
+    // Constructores
     Libro();
+    /**
+     * Constructor con parámetros
+     * inicializar el estado del objeto 
+     * @param t Título del libro
+     * @param a Autor del libro
+     * @param n Año de publicación
+     *  */ 
+
     Libro(string t, string a, int n);
 
-    // Getters
+    // Getters para acceder a los atributos
     string getTitulo() const;
     string getAutor() const;
     int getAnio() const;
+    /**
+     * Devuelve la info base del libro en formato string
+     * (estado del objeto )
+     * @return string con la info del libro
+     */
 
     // Métodos
     string toString() const; 
 };
 
-// Implementación
+/**
+ * Implementación de las funciones ya definicas
+ *  */
+
 
 Libro::Libro() {
     titulo = "";
@@ -43,16 +62,21 @@ Libro::Libro(string t, string a, int n) {
 
 // Getters
 
-string Libro::getTitulo() const { return titulo; }
-string Libro::getAutor() const { return autor; }
-int Libro::getAnio() const { return anio; }
+string Libro::getTitulo() const { 
+    return titulo; 
+}
+string Libro::getAutor() const { 
+    return autor; 
+}
+int Libro::getAnio() const {
+     return anio; 
+}
 
+// Métodos
 
 string Libro::toString() const {
     stringstream aux;
-    aux << "Título: " << titulo << "\n"
-        << "Autor: " << autor << "\n"
-        << "Año: " << anio;
+    aux << "Título: " << titulo<< "Autor: " << autor << "Año: " << anio;
     return aux.str();
 }
 
